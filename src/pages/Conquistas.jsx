310130d8eff1374abb75f60e88d522ca6ac2cf69
Anark <dreckmannmoises@gmail.com>
2024-02-28 21:21:25 -0300

    up

--- src/pages/Conquistas.jsx
+++ src/pages/Conquistas.jsx
@@ -3,12 +3,31 @@ import Footer from "../components/Footer.jsx"
 
 import santander from '../assets/imagesPage/santander.png'
 import ifood from '../assets/imagesPage/ifood.jpg'
+import formacao1 from '../assets/imagesPage/formacao1.png'
+import formacao2 from '../assets/imagesPage/formacao2.png'
+import formacao3 from '../assets/imagesPage/formacao3.png'
 
 function Conquistas() {
     return(
         <>
             <main className="containerFrontBack">
                 <Navbar className="nav" />
+                <div className='containerConquista'>
+                    <h2>FORMAÇÃO EM REACT NATIVE</h2>
+                    <img src={formacao3} alt="imagem da conquista" className='imgconquista'/>
+                </div>
+
+                <div className='containerConquista'>
+                    <h2>FORMAÇÃO EM TYPESCRIPT</h2>
+                    <img src={formacao2} alt="imagem da conquista" className='imgconquista'/>
+                </div>
+
+                <div className='containerConquista'>
+                    <h2>FORMAÇÃO EM REACT</h2>
+
+                    <img src={formacao1} alt="imagem da conquista" className='imgconquista'/>
+                </div>
+
                 <div className='containerConquista'>
                     <h2>2° colocado bootcamp Ifood Tech Desenvolvimento de Jogos com Javascript</h2>
                     <p>16921 participantes | Bootcamp foi de 25/09/23 até 19/02/24</p>
